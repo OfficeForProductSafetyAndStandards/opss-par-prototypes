072c9d9b3ce7ad3a194521f661c81568106fc0e4
Richard priddy <richard.priddy@triad.co.uk>
2024-09-19 11:56:00 +0100

    Deal with star rating on pages

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -5,4 +5,19 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+  const radioButtons = document.querySelectorAll('.star-rating .govuk-radios__input');
+
+  radioButtons.forEach(radio => {
+    radio.addEventListener('change', function () {
+      // Remove "before-selected" class from all labels
+      document.querySelectorAll('.govuk-label').forEach(label => label.classList.remove('govuk-radios__label_before_selected'));
+
+      // Add "before-selected" class to all previous labels
+      let previousSibling = this.closest('.govuk-radios__item').previousElementSibling;
+      while (previousSibling) {
+        previousSibling.querySelector('.govuk-label').classList.add('govuk-radios__label_before_selected');
+        previousSibling = previousSibling.previousElementSibling;
+      }
+    });
+  });
 })
